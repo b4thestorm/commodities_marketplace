1649c64ef2311f4181d247315296526e695f154d
arnoldsanders <arnoldsander@gmail.com>
2020-08-20 19:55:20 -0400

    many to many self referential joins and initial models

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  #Design decision conflict - wanted to add a table constraint to only add [buyer, seller] user types
+  
+end
