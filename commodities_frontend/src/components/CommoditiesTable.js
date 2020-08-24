31ab8394629cc6a4a3d29ac9e6627236e1689935
arnoldsanders <arnoldsander@gmail.com>
2020-08-23 22:29:06 -0400

    style table and fetch commodities

--- commodities_frontend/src/components/CommoditiesTable.js
+++ commodities_frontend/src/components/CommoditiesTable.js
@@ -1,40 +1,42 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
+import Table from 'react-bootstrap/Table'
 import axios from 'axios'
 
-class CommoditiesTable extends React.Component {
-  useEffect(){
-    let user = JSON.parse(localStorage.getItem('user'));
+function CommoditiesTable() {
+  // const [resourceType, setResourceType] = useState('posts')
+  const [items, setItems] = useState([])
+  useEffect(() => {
+    let user = JSON.parse(localStorage.getItem('user'))
     axios({
       method: 'GET',
-      url: 'http://localhost:3001/user/18/commodities',
+      url: 'http://localhost:3001/users/18/commodities',
       headers: {'access-token': user['access-token'], 'client': user['client'], 'uid': user['uid']}
     })
-    .then(response => {  setEvent(response.data)})
-    .catch(err => err.message);
-  }
+    .then(response => { setItems(response.data);})
+  }, [])
 
-render() {
-  return(
-    //TODO: get data from api
-    //TODO: parse data into table
-    //TODO: add link to table
-    //TODO: add table to it's own view
-    
-    <table onChange={this.getList}>
-    <tr>
-      <th>Product Name</th>
-      <th>Quantity</th>
-      <th>Price</th>
-    </tr>
-    <tr>
-      <td>Bill Gates</td>
-      <td>55577854</td>
-      <td>55577855</td>
-    </tr>
-  </table>
+  return (
+
+        <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Product Name</th>
+            <th>Qty</th>
+            <th>Price(cents)</th>
+          </tr>
+        </thead>
+        <tbody>
+        {items.map((item, i) => {
+          return (
+           <tr key={i}>
+            <td>{item.product_name}</td>
+            <td>{item.quantity}</td>
+            <td>{item.price}</td>
+          </tr>)})}
+        </tbody>
+        </Table>
   )
- }
 }
 
-export default CommoditiesTable
+export default CommoditiesTable;
